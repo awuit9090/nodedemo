f0981b808b7471eceac6a410df66d898cbc0fa67
Arong Wu <awuit9090@gmail.com>
2018-04-17 18:00:02 -0400

    v1.0

--- demo_mongodb_find_fields.js
+++ demo_mongodb_find_fields.js
@@ -6,7 +6,9 @@ MongoClient.connect(url, (err, db) => {
   if (err) throw err;
   var dbo = db.db("mydb");
   //Exclude the _id field from the result:
-  dbo.collection("customers").find({}, { _id: 0, name: 1, adress: 1 }).toArray((err, result) => {
+  dbo.collection("customers").find(
+    { }, {projection: { _id: 0 }}
+  ).toArray((err, result) => {
     if (err) throw err;
     console.log(result);
     db.close();
