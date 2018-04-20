65be67f85ceea77d1e4a69e85271725f51c2976d
Arong Wu <awuit9090@gmail.com>
2018-04-19 20:24:45 -0400

    v1.0

---
+++ demo_mongodb_delete_many.js
@@ -0,0 +1,14 @@
+var MongoClient = require('mongodb').MongoClient;
+
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, (err, db) => {
+  if (err) throw err;
+    var dbo = db.db("mydb");
+      var myquery = { address: /^O/ };
+        dbo.collection("customers").deleteMany(myquery, (err, obj) => {
+	    if (err) throw err;
+	      console.log(obj.result.n + " document(s) deleted");
+		    db.close();
+		      });
+		      });
---
+++ demo_mongodb_query_s.js
@@ -0,0 +1,14 @@
+var MongoClient = require('mongodb').MongoClient;
+
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, (err, db) => {
+  if (err) throw err;
+    var dbo = db.db("mydb");
+      var query = { address: /^S/ };
+        dbo.collection("customers").find(query).toArray((err, result) => {
+	    if (err) throw err;
+	        console.log(result);
+		    db.close();
+		      });
+		      });
